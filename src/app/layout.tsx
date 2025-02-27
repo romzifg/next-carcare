3cfa244dcfd0958656f15a0254722b8e308f90c1
Romzi Farhan Ghozi <romzifg@gmail.com>
2025-02-27 11:42:28 +0700

    update: install swiper js and clear image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "@/assets/styles/global.css";
 import "@/assets/styles/output.css";
+import 'swiper/css';
 
 const getPoppins = Poppins({
   variable: '--font-poppins',
